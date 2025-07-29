339ca2ab6dbad5da4e67dd8a23f1cae2c2f88dea
Nguyễn Hà Minh Quân <quannguyenhaminh18@gmail.com>
2025-07-29 17:00:17 +0700

    create language switcher component

--- src/components/sections/Header.tsx
+++ src/components/sections/Header.tsx
@@ -3,7 +3,7 @@
 import React from "react";
 import {useTranslations} from 'next-intl';
 import {Link, usePathname} from '@/i18n/navigation';
-
+import LanguageSwitcher from "@/components/LanguageSwitcher";
 
 const LINKS = [
     { key: "me.sortName", href: "/" },
@@ -19,19 +19,25 @@ const Header = () => {
 
     return (
         <div className="w-full fixed top-0 left-0 z-50 bg-black/70 backdrop-blur-md px-4">
-            <nav className="max-w-screen-xl mx-auto h-[60px] flex items-center justify-center gap-6">
-                {LINKS.map((link, index) => (
-                    <Link
-                        key={index}
-                        href={link.href}
-                        className={`text-2xl text-zinc-500 hover:text-zinc-300 transition-colors ${
-                            activeRoute === link.href ? "text-zinc-200" : ""
-                        }`}
-                    >
-                        {t(link.key)}
-                    </Link>
-                ))}
-            </nav>
+            <div className="max-w-screen-xl mx-auto h-[60px] flex items-center justify-between">
+                {/* Navigation Links */}
+                <nav className="flex items-center gap-6">
+                    {LINKS.map((link, index) => (
+                        <Link
+                            key={index}
+                            href={link.href}
+                            className={`text-2xl text-zinc-500 hover:text-zinc-300 transition-colors ${
+                                activeRoute === link.href ? "text-zinc-200" : ""
+                            }`}
+                        >
+                            {t(link.key)}
+                        </Link>
+                    ))}
+                </nav>
+                <div>
+                    <LanguageSwitcher />
+                </div>
+            </div>
         </div>
     );
 };
