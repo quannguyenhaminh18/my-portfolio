e2e12ee6f25591062cb05ef7fdef3b3564f88ab2
Nguyễn Hà Minh Quân <quannguyenhaminh18@gmail.com>
2025-07-31 15:42:21 +0700

    remove trash

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -2,7 +2,6 @@
 import { useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import Link from "next/link";
-import styles from "./style.module.scss";
 import { opacity, background } from "./anim";
 import Nav from "./nav";
 import { cn } from "@/lib/utils";
@@ -15,68 +14,82 @@ interface HeaderProps {
 
 const Header = ({ loader }: HeaderProps) => {
   const [isActive, setIsActive] = useState<boolean>(false);
+
   return (
-    <motion.header
-      className={cn(
-        styles.header,
-        "transition-colors delay-100 duration-500 ease-in"
-      )}
-      style={{
-        background: isActive ? "hsl(var(--background) / .8)" : "transparent",
-      }}
-      initial={{
-        y: -80,
-      }}
-      animate={{
-        y: 0,
-      }}
-      transition={{
-        delay: loader ? 3.5 : 0,
-        duration: 0.8,
-      }}
-    >
-      <div className={cn(styles.bar, "flex items-center justify-between")}>
-        <Link href="/" className="flex items-center justify-center">
-          <Button variant={"link"} className="text-md">
-            {config.author}
-          </Button>
-        </Link>
-        <Button
-          variant={"ghost"}
-          onClick={() => setIsActive(!isActive)}
+      <motion.header
           className={cn(
-            styles.el,
-            "m-0 p-0 h-6 bg-transparent flex items-center justify-center"
+              "fixed w-full z-[9] box-border p-4 sm:p-5 backdrop-blur-lg transition-colors duration-500 ease-in",
+              isActive ? "bg-background/80" : "bg-transparent"
           )}
-        >
-          <div className="relative flex items-center">
-            <motion.p
-              variants={opacity}
-              animate={!isActive ? "open" : "closed"}
+          initial={{ y: -80 }}
+          animate={{ y: 0 }}
+          transition={{
+            delay: loader ? 3.5 : 0,
+            duration: 0.8,
+          }}
+      >
+        <div className="relative flex items-center justify-between lowercase text-[12px] sm:text-[15px] font-normal">
+          {/* Left: Author */}
+          <Link href="/" className="absolute left-0">
+            <Button variant="link" className="text-md">
+              {config.author}
+            </Button>
+          </Link>
+
+          {/* Right: Toggle Button */}
+          <div className="absolute right-0 flex gap-8">
+            <div className="hidden sm:block cursor-pointer">Shop</div>
+
+            <Button
+                variant="ghost"
+                onClick={() => setIsActive(!isActive)}
+                className="m-0 p-0 h-6 bg-transparent flex items-center justify-center gap-3 cursor-pointer"
             >
-              Menu
-            </motion.p>
-            <motion.p variants={opacity} animate={isActive ? "open" : "closed"}>
-              Close
-            </motion.p>
+              <div className="relative flex items-center">
+                <motion.p variants={opacity} animate={!isActive ? "open" : "closed"}>
+                  Menu
+                </motion.p>
+                <motion.p
+                    variants={opacity}
+                    animate={isActive ? "open" : "closed"}
+                    className="absolute opacity-0"
+                >
+                  Close
+                </motion.p>
+              </div>
+
+              {/* Burger Icon */}
+              <div className="relative w-[22.5px] pointer-events-none">
+              <span
+                  className={cn(
+                      "block h-px w-full bg-foreground transition-all duration-1000 ease-[cubic-bezier(0.76,0,0.24,1)] relative",
+                      isActive ? "rotate-45 top-[-1px]" : "top-[-4px]"
+                  )}
+              />
+                <span
+                    className={cn(
+                        "block h-px w-full bg-foreground transition-all duration-1000 ease-[cubic-bezier(0.76,0,0.24,1)] relative",
+                        isActive ? "-rotate-45 top-[1px]" : "top-[4px]"
+                    )}
+                />
+              </div>
+            </Button>
           </div>
-          <div
-            className={`${styles.burger} ${
-              isActive ? styles.burgerActive : ""
-            }`}
-          ></div>
-        </Button>
-      </div>
-      <motion.div
-        variants={background}
-        initial="initial"
-        animate={isActive ? "open" : "closed"}
-        className={styles.background}
-      ></motion.div>
-      <AnimatePresence mode="wait">
-        {isActive && <Nav setIsActive={setIsActive} />}
-      </AnimatePresence>
-    </motion.header>
+        </div>
+
+        {/* Background Overlay Animation */}
+        <motion.div
+            variants={background}
+            initial="initial"
+            animate={isActive ? "open" : "closed"}
+            className="absolute left-0 top-full w-full h-full bg-[hsl(240,10%,3.9%)] opacity-50"
+        ></motion.div>
+
+        {/* Mobile Nav (conditionally rendered) */}
+        <AnimatePresence mode="wait">
+          {isActive && <Nav setIsActive={setIsActive} />}
+        </AnimatePresence>
+      </motion.header>
   );
 };
 
