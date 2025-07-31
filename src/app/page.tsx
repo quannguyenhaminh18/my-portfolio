f3a2ffbf6c80499039672a5c59ef8927d251f0a2
Nguyễn Hà Minh Quân <quannguyenhaminh18@gmail.com>
2025-07-31 14:05:06 +0700

    remove trash

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@
 import React from "react";
 import SmoothScroll from "@/components/smooth-scroll";
 import { cn } from "@/lib/utils";
-import AnimatedBackground from "@/components/animated-background";
 import SkillsSection from "@/components/sections/skills";
 import ProjectsSection from "@/components/sections/projects";
 import ContactSection from "@/components/sections/contact";
@@ -14,9 +13,6 @@ function MainPage() {
     <>
       <SmoothScroll>
         <main className={cn("bg-slate-100 dark:bg-transparent")}>
-          <div className="top-0 z-0 fixed w-full h-screen">
-            <AnimatedBackground />
-          </div>
           <HeroSection />
           <SkillsSection />
           <ProjectsSection />
